e7638bb757b3ce9a6decd40ff57d438a7e99b924
Besenok-13 <artbondar2003@gmail.com>
2022-11-18 17:40:47 +0300

    eslint

--- static/js/game.js
+++ static/js/game.js
@@ -1,78 +1,59 @@
-var i_object1 = { value: 0, now_rot: 0 };
-var i_object2 = { value: 0, now_rot: 0 };
-var i_object3 = { value: 0, now_rot: 0 };
-var i_object4 = { value: 0, now_rot: 0 };
-var i_object5 = { value: 0, now_rot: 0 };
-var i_object6 = { value: 0, now_rot: 0 };
+function start(obj, event) {
+  obj.style.position = 'absolute';
+  obj.style.zIndex = 1000;
+  document.body.append(obj);
+  if (event.which === 1) {
+    let shiftX = 0;
+    let shiftY = 0;
+    // тоже нужно переделать для произвольного угла
+    shiftX = event.clientX + window.pageXOffset - (parseInt(obj.style.left));
+    shiftY = event.clientY + window.pageYOffset - (parseInt(obj.style.top));
+
+    // moveAt(event.pageX, event.pageY);
+    // переносит мяч на координаты (pageX, pageY),
+    // дополнительно учитывая изначальный сдвиг относительно указателя мыши
+    function moveAt(pageX, pageY) {
+      obj.style.left = pageX - shiftX + 'px';
+      obj.style.top = pageY - shiftY + 'px';
+    };
+
+    function onMouseMove(event) {
+      moveAt(event.pageX, event.pageY);
+    }
+  };
+
+  // передвигаем мяч при событии mousemove
+  document.addEventListener('mousemove', onMouseMove);
+  // отпустить мяч, удалить ненужные обработчики
+  document.onmouseup = function () {
+    document.removeEventListener('mousemove', onMouseMove);
+    obj.onmouseup = null;
+    obj.style.zIndex = 1;
+  };
+
+  obj.ondragstart = function () {
+    return false;
+  };
+}
 
 object1.onmousedown = function (event) {
-    start(object1, event, i_object1);
+  start(object1, event);
 };
 
 object2.onmousedown = function (event) {
-    start(object2, event, i_object2);
+  start(object2, event);
 };
 
 object3.onmousedown = function (event) {
-    start(object3, event, i_object3);
+  start(object3, event);
 };
 
 object4.onmousedown = function (event) {
-    start(object4, event, i_object4);
+  start(object4, event);
 };
 object5.onmousedown = function (event) {
-    start(object5, event, i_object5);
+  start(object5, event);
 };
 object6.onmousedown = function (event) {
-    start(object6, event, i_object6);
+  start(object6, event);
 };
-
-
-
-
-function start(obj, event, j) {
-    i = j.value
-    obj.style.position = 'absolute';
-    obj.style.zIndex = 1000;
-    document.body.append(obj);
-    if (event.which == 1) {
-        var shiftX = 0;
-        var shiftY = 0;
-        //тоже нужно переделать для произвольного угла
-        shiftX = event.clientX + window.pageXOffset - (parseInt(obj.style.left));
-        shiftY = event.clientY + window.pageYOffset - (parseInt(obj.style.top));
-        console.log(shiftX, shiftY)
-
-
-        //moveAt(event.pageX, event.pageY);
-
-        // переносит мяч на координаты (pageX, pageY),
-        // дополнительно учитывая изначальный сдвиг относительно указателя мыши
-        function moveAt(pageX, pageY) {
-            obj.style.left = pageX - shiftX + 'px';
-            obj.style.top = pageY - shiftY + 'px';
-        };
-
-        function onMouseMove(event) {
-
-            moveAt(event.pageX, event.pageY);
-        };
-    };
-
-
-
-
-
-    // передвигаем мяч при событии mousemove
-    document.addEventListener('mousemove', onMouseMove);
-    // отпустить мяч, удалить ненужные обработчики
-    document.onmouseup = function () {
-        document.removeEventListener('mousemove', onMouseMove);
-        obj.onmouseup = null;
-        obj.style.zIndex = 1
-    };
-
-    obj.ondragstart = function () {
-        return false;
-    };
-}
